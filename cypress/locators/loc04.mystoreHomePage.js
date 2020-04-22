2787fd8e83de7367c295f6a80f573e9a3c97af76
sachinjadhav222 <sachin.jadhav222@yahoo.com>
2020-04-22 17:55:19 +0100

    changes to package

--- cypress/locators/loc04.mystoreHomePage.js
+++ cypress/locators/loc04.mystoreHomePage.js
@@ -3,8 +3,9 @@ module.exports={
    loc04_signIn_button: "a.login",
    loc04_create_email: "#email_create",
    loc04_create_submit_button: "#SubmitCreate",
-   loc04_account_creation_form: "#account-creation_form",
+   //loc04_account_creation_form: "#account-creation_form",
    loc04_create_account_heading: '//h1[contains(text(),"Create an account")]',
-   loc04_first_name: "#customer_firstname",
+  // loc04_first_name: "#customer_firstname",
+   loc04_account_creation_form: "h1.page-heading",
 
 }
