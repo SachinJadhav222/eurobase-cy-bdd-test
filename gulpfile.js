f6a33e622751ec82a72cfba2a70e5b04d8e5b8f3
sachin.jadhav222 <sachin.jadhav222@yahoo.com>
2020-12-03 13:03:36 +0000

    gulp file

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const gulp = require('gulp');
+const {src, series, parallel,dest,watch} = require('gulp');
+
+function myName(){
+  console.log('This is Gulp file from Function ===================>')
+}
+
+function copyFiles() {
+    console.log('This is copy file ===================>')
+    return src('./locators/*.js').pipe(gulp.dest('./gilpDestination'))
+}
+
+
+
+exports.default=myName;
+
+gulp.task('myName1',(done)=>{
+    console.log('This is Gulp Task file  May name 1 ===================>');
+  //  return src('.*.js').pipe(gulp.dest('./locators/gilpDestination'))
+
+})
+
+gulp.task('myName2',(done)=>{
+    console.log('This is Gulp Task file Myname-2 ===================>');
+   // return src('.*.js').pipe(gulp.dest('./locators/gilpDestination'))
+
+})
+gulp.task('myName3',['myName2','myName1'],(done)=>{
+    console.log('This is Gulp Task file- myName-3 ===================>');
+   
+
+})
